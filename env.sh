6294a12d3939150f007d2e6c0f6e9a4707da3f69
silgon <silgon3200@gmail.com>
2014-09-02 19:09:10 +0200

    history saving commented because i was having some problems with that  - needs later solution python path deleted because i'm using commands now

--- env.sh
+++ env.sh
@@ -18,14 +18,14 @@ alias tmux="TERM=xterm-256color tmux" # tmux with colors
 # opencv
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-# history for multiple terminal programs such as yakuake, tmux, terminator, etc
-# avoid duplicates..
-export HISTCONTROL=ignoredups:erasedups  
-# append history entries..
-shopt -s histappend
+# # history for multiple terminal programs such as yakuake, tmux, terminator, etc
+# # avoid duplicates..
+# export HISTCONTROL=ignoredups:erasedups  
+# # append history entries..
+# shopt -s histappend
 
-# After each command, save and reload history
-export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+# # After each command, save and reload history
+# export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
 
 
 # export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3:/usr/local/lib/python3/dist-packages
