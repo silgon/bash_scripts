6a37f861b2d93bc01e8db9f4d9126e8aba631419
silgon <silgon3200@gmail.com>
2015-11-21 19:54:37 +0100

    change conda PATH variable so it can run python from the conda package

--- conda/use_conda.sh
+++ conda/use_conda.sh
@@ -17,7 +17,7 @@ if [ -d $CONDA_INSTALL_PATH ]
 then
     if [ $# -eq 1 ]
     then
-        export PATH=$PATH:$CONDA_INSTALL_PATH/2/bin
+        export PATH=$CONDA_INSTALL_PATH/2/bin:$PATH
         echog "using version default anaconda 2"
     elif [ $# -eq 2 ]
     then
@@ -25,7 +25,7 @@ then
         export CONDA_DEFAULT_ENV="${2}-default"
         echog "using anaconda $2"
     else
-        export PATH=$PATH:$CONDA_BASE_PATH/$2/envs/$3/bin
+        export PATH=$CONDA_BASE_PATH/$2/envs/$3/bin:$PATH
         export CONDA_DEFAULT_ENV=$3
         echog "using anaconda $2, environment $3"
     fi
