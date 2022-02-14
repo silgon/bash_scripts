db298f0b2b1ba2384afd0d7b59d613b20d4ca647
silgon <silgon3200@gmail.com>
2022-02-14 14:38:32 +0100

    change|use_conda: new way to call use conda

--- conda/use_conda.sh
+++ conda/use_conda.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# find out ros base and install path
 source ${BASH_PATH}/conda/conda.sh
+source <(${CONDA_INSTALL_PATH}/bin/conda shell.bash hook| sed '$d')
+
+
 
 # this keeps the variable in path
-# TODO(silgon): this if should be changed to a smarter way
 # seeking for the anaconda path
 if [[ -z $OLD_PATH ]]; then
     export OLD_PATH=$PATH
@@ -17,13 +18,12 @@ if [ -d $CONDA_INSTALL_PATH ]
 then
     if [ $# -eq 1 ]
     then
-        export PATH=$CONDA_INSTALL_PATH/bin:$PATH
         export CONDA_DEFAULT_ENV="base"
+        eval "$(__conda_exe shell.posix activate | sed '/PS1=/d')"
         echog "using version default anaconda"
     else
-        export PATH=$CONDA_INSTALL_PATH/bin:$PATH
-        export PATH=$CONDA_BASE_PATH/envs/$2/bin:$PATH
         export CONDA_DEFAULT_ENV="$2"
+        eval "$(__conda_exe shell.posix activate $2 | sed '/PS1=/d')"
         echog "using anaconda $2, environment $3"
     fi
     export CONDA_VERSION=$(python --version  2>&1  | grep -oP "\d\.\d")
